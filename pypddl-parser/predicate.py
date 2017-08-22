13f0755be5631f4c388886816aaa067787477d77
Thiago P. Bueno <thiago.pbueno@gmail.com>
2017-08-22 20:14:12 -0300

    Add predicate.py

---
+++ pypddl-parser/predicate.py
@@ -0,0 +1,56 @@
+# This file is part of pypddl-parser.
+
+# pypddl-parser is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# pypddl-parser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with pypddl-parser.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from term import Term
+
+
+class Predicate(object):
+
+    def __init__(self, name, args=[]):
+        self._name = name
+        self._args = args
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def args(self):
+        return self._args[:]
+
+    @property
+    def arity(self):
+        return len(self._args)
+
+    def is_grounded(self):
+        return all( arg.is_constant() for arg in self._args )
+
+    def ground(self, subst):
+        args = []
+        for arg in self._args:
+            if arg in subst:
+                value = subst[arg]
+                arg = Term.constant(value)
+            args.append(arg)
+        return Predicate(self._name, args)
+
+    def __str__(self):
+        if self._name == '=':
+            return '{0} = {1}'.format(str(self._args[0]), str(self._args[1]))
+        elif self.arity == 0:
+            return self._name
+        else:
+            return '{0}({1})'.format(self._name, ', '.join(map(str, self._args)))
