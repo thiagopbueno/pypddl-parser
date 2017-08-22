932c582f1528b67796379ba9f9773bd21be4ea3d
Thiago P. Bueno <thiago.pbueno@gmail.com>
2017-08-22 20:14:55 -0300

    Add action.py

---
+++ pypddl-parser/action.py
@@ -0,0 +1,58 @@
+# This file is part of pypddl-parser.
+
+# pypddl-parser is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# pypddl-parser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with pypddl-parser.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class Action(object):
+
+    def __init__(self, name, params, precond, effects):
+        self._name    = name
+        self._params  = params
+        self._precond = precond
+        self._effects = effects
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def params(self):
+        return self._params[:]
+
+    @property
+    def precond(self):
+        return self._precond[:]
+
+    @property
+    def effects(self):
+        return self._effects[:]
+
+    def instantiate(self, subst):
+        args = [ str(subst.get(param.name, param)) for param in self._params ]
+        precond = { str(l.predicate.ground(subst)) for l in self._precond if l.is_positive() }
+        pos_effect = set()
+        neg_effect = set()
+        for eff in self._effects:
+            ground_predicate = eff.predicate.ground(subst)
+            if eff.is_positive():
+                pos_effect.add(str(ground_predicate))
+            elif eff.is_negative():
+                neg_effect.add(str(ground_predicate))
+        return Action(self._name, args, precond, pos_effect, neg_effect)
+
+    def __str__(self):
+        operator_str  = '{0}({1})\n'.format(self._name, ', '.join(map(str, self._params)))
+        operator_str += '>> precond: {0}\n'.format(', '.join(map(str, self._precond)))
+        operator_str += '>> effects: {0}\n'.format(', '.join(map(str, self._effects)))
+        return operator_str
