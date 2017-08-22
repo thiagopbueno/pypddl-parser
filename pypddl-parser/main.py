df3ed110d3c856aea4b2f979d708a0b58172bdd4
Thiago P. Bueno <thiago.pbueno@gmail.com>
2017-08-22 20:16:38 -0300

    Add main.py

---
+++ pypddl-parser/main.py
@@ -0,0 +1,40 @@
+# This file is part of pypddl-PDDLParser.
+
+# pypddl-parser is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# pypddl-parser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with pypddl-parser.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import argparse
+
+from pddlparser import PDDLParser
+
+
+def parse():
+    usage = 'python3 main.py <DOMAIN> <INSTANCE>'
+    description = 'pypddl-parser is a PDDL parser built on top of ply.'
+    parser = argparse.ArgumentParser(usage=usage, description=description)
+
+    parser.add_argument('domain',  type=str, help='path to PDDL domain file')
+    parser.add_argument('problem', type=str, help='path to PDDL problem file')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse()
+
+    domain  = PDDLParser.parse(args.domain)
+    problem = PDDLParser.parse(args.problem)
+
+    print(domain)
+    print(problem)
